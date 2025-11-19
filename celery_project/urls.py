73aa0e0812eb90fc63b4e4e477addc9c2bcc8b50
fazil <muhammedfazil00@gmail.com>
2025-11-19 14:28:59 +0530

    admin ui fix

--- celery_project/urls.py
+++ celery_project/urls.py
@@ -3,9 +3,18 @@ URL configuration for celery_project project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
 ]
+
+# Serve static files in development and production
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # In production, serve static files from STATIC_ROOT
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
